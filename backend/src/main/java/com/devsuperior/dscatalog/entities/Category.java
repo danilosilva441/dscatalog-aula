dc721adecfea2869c438d34ec1f7a1f5983fe91c
Danilo da Silva Souza <danilosilva441@gmail.com>
2021-12-16 19:53:39 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,12 +2,15 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -29,6 +32,9 @@ public class Category implements Serializable {
 	// Hora exata em que fez a aplicação, no horario universal UTC
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
+
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 
 	public Category() {
 	}
@@ -61,18 +67,21 @@ public class Category implements Serializable {
 	public Instant getUpdatedAt() {
 		return updatedAt;
 	}
-	
-	
-	//sempre que chamar um save no banco ele chama esse quando for criado
+
+	// sempre que chamar um save no banco ele chama esse quando for criado
 	@PrePersist
 	public void prePersist() {
 		createdAt = Instant.now();
 	}
-	
-	//sempre que atualizar um save no banco ele chama esse
+
+	// sempre que atualizar um save no banco ele chama esse
 	@PreUpdate
 	public void preUpdate() {
 		updatedAt = Instant.now();
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
